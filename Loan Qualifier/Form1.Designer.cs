9b75d0e11df6d53d7c044bd1ac9ba1ff74301dd3
TantumErgo <9907196+TantumErgo@users.noreply.github.com>
2019-06-28 19:53:17 -0400

    Add project files.

---
+++ Loan Qualifier/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace Loan_Qualifier
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.annualSalaryLabel = new System.Windows.Forms.Label();
+            this.yearsLabel = new System.Windows.Forms.Label();
+            this.loanDecisionLabel = new System.Windows.Forms.Label();
+            this.decisionLabel = new System.Windows.Forms.Label();
+            this.salaryTextBox = new System.Windows.Forms.TextBox();
+            this.yearsTextBox = new System.Windows.Forms.TextBox();
+            this.checkButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // annualSalaryLabel
+            // 
+            this.annualSalaryLabel.AutoSize = true;
+            this.annualSalaryLabel.Location = new System.Drawing.Point(27, 27);
+            this.annualSalaryLabel.Name = "annualSalaryLabel";
+            this.annualSalaryLabel.Size = new System.Drawing.Size(72, 13);
+            this.annualSalaryLabel.TabIndex = 0;
+            this.annualSalaryLabel.Text = "Annual Salary";
+            // 
+            // yearsLabel
+            // 
+            this.yearsLabel.AutoSize = true;
+            this.yearsLabel.Location = new System.Drawing.Point(27, 76);
+            this.yearsLabel.Name = "yearsLabel";
+            this.yearsLabel.Size = new System.Drawing.Size(103, 13);
+            this.yearsLabel.TabIndex = 1;
+            this.yearsLabel.Text = "Years at Current Job";
+            // 
+            // loanDecisionLabel
+            // 
+            this.loanDecisionLabel.AutoSize = true;
+            this.loanDecisionLabel.Location = new System.Drawing.Point(27, 140);
+            this.loanDecisionLabel.Name = "loanDecisionLabel";
+            this.loanDecisionLabel.Size = new System.Drawing.Size(75, 13);
+            this.loanDecisionLabel.TabIndex = 2;
+            this.loanDecisionLabel.Text = "Loan Decision";
+            // 
+            // decisionLabel
+            // 
+            this.decisionLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.decisionLabel.Location = new System.Drawing.Point(30, 166);
+            this.decisionLabel.Name = "decisionLabel";
+            this.decisionLabel.Size = new System.Drawing.Size(242, 23);
+            this.decisionLabel.TabIndex = 3;
+            this.decisionLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // salaryTextBox
+            // 
+            this.salaryTextBox.Location = new System.Drawing.Point(172, 24);
+            this.salaryTextBox.Name = "salaryTextBox";
+            this.salaryTextBox.Size = new System.Drawing.Size(100, 20);
+            this.salaryTextBox.TabIndex = 4;
+            // 
+            // yearsTextBox
+            // 
+            this.yearsTextBox.Location = new System.Drawing.Point(172, 73);
+            this.yearsTextBox.Name = "yearsTextBox";
+            this.yearsTextBox.Size = new System.Drawing.Size(100, 20);
+            this.yearsTextBox.TabIndex = 5;
+            // 
+            // checkButton
+            // 
+            this.checkButton.Location = new System.Drawing.Point(3, 209);
+            this.checkButton.Name = "checkButton";
+            this.checkButton.Size = new System.Drawing.Size(78, 41);
+            this.checkButton.TabIndex = 6;
+            this.checkButton.Text = "Check Qualifications";
+            this.checkButton.UseVisualStyleBackColor = true;
+            this.checkButton.Click += new System.EventHandler(this.checkButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(99, 209);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(75, 23);
+            this.clearButton.TabIndex = 7;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(197, 209);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.TabIndex = 8;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.checkButton);
+            this.Controls.Add(this.yearsTextBox);
+            this.Controls.Add(this.salaryTextBox);
+            this.Controls.Add(this.decisionLabel);
+            this.Controls.Add(this.loanDecisionLabel);
+            this.Controls.Add(this.yearsLabel);
+            this.Controls.Add(this.annualSalaryLabel);
+            this.Name = "Form1";
+            this.Text = "Loan Qualifier";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label annualSalaryLabel;
+        private System.Windows.Forms.Label yearsLabel;
+        private System.Windows.Forms.Label loanDecisionLabel;
+        private System.Windows.Forms.Label decisionLabel;
+        private System.Windows.Forms.TextBox salaryTextBox;
+        private System.Windows.Forms.TextBox yearsTextBox;
+        private System.Windows.Forms.Button checkButton;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
